75474b555ee5cf8deaefb5932d94770b0f2accf1
MantasPetrosiusI <mantas.petrosius@outlook.com>
2023-05-11 15:41:15 +0100

    ......

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,9 @@ import server from "./server";
 
 const port = process.env.PORT || 3001;
 
-mongoose.connect(process.env.MONGO_URL as string);
+if (process.env.MONGO_URL) {
+  mongoose.connect(process.env.MONGO_URL as string);
+}
 
 mongoose.connection.on("connected", () => {
   server.listen(port, () => {
@@ -12,5 +14,3 @@ mongoose.connection.on("connected", () => {
     console.log(`Server listening on port ${port}`);
   });
 });
-
-export default {};
